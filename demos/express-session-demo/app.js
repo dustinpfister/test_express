3c09d162520b888d05957790ff08c38c6ac5f613
dustinpfister <dustin.pfister@gmail.com>
2018-06-01 11:34:14 -0400

    comments

--- demos/express-session-demo/app.js
+++ demos/express-session-demo/app.js
@@ -1,17 +1,18 @@
 let express = require('express'),
-
 port = process.env.PORT || process.argv[2] || 8080,
 app = express();
 
+// using express-session
 app.use(require('express-session')({
 
-        name: '_es_demo',
-        secret: '1234',
-        resave: false,
-        saveUninitialized: false,
+        name: '_es_demo', // The name of the cookie
+        secret: '1234', // The secret is required, and is used for signing cookies
+        resave: false, // Force save of session for each request.
+        saveUninitialized: false // Save a session that is new, but has not been modified
 
     }));
 
+// single path for root
 app.get('/', function (req, res) {
 
     // simple count for the session
@@ -20,10 +21,12 @@ app.get('/', function (req, res) {
     }
     req.session.count += 1;
 
+    // respond with the session object
     res.json(req.session);
 
 });
 
+// start the server
 app.listen(port, function () {
 
     console.log('express-session demo is up on port: ' + port);
