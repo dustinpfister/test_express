07bd9557f2edd0541df5482e6b6a49be47704a9c
dustinpfister <dustin.pfister@gmail.com>
2018-06-05 11:09:12 -0400

    app.all demo

---
+++ demos/app-all/app.js
@@ -0,0 +1,21 @@
+let express = require('express'),
+
+port = process.env.PORT || process.argv[2] || 8080,
+
+app = express();
+
+app.all('*', function (req, res, next) {
+
+    console.log(req.method);
+
+    next();
+
+});
+
+app.use('/', express.static('public'));
+
+app.listen(port, function () {
+
+    console.log('app.all demo is up on port: ' + port);
+
+});
