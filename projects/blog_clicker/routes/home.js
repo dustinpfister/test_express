bcbb3c589585a62bd2e440207fed99db8f716268
dustinpfister <dustin.pfister@gmail.com>
2018-06-05 17:10:24 -0400

    posts show up on homepage

--- projects/blog_clicker/routes/home.js
+++ projects/blog_clicker/routes/home.js
@@ -8,9 +8,7 @@ posts = require('../lib/posts');
 // get hard coded demo posts in the database at /db/posts.json
 router.get('/', function (req, res) {
 
-    let userPosts = posts.getOwnedPosts(req.user.id);
-
-    console.log(userPosts.value());
+    let userPosts = posts.getOwnedPosts(req.user.id).value();
 
     res.render('index', {
         layout: 'home',
