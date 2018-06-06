97b3b5d16438a575cb8275b002def1f933202e58
dustinpfister <dustin.pfister@gmail.com>
2018-06-06 13:34:56 -0400

    working out a client

--- projects/micro_img_main/app.js
+++ projects/micro_img_main/app.js
@@ -5,6 +5,17 @@ app = express();
 
 app.use('/', express.static('public'));
 
+app.use(require('body-parser').raw());
+app.post('/', function (req, res) {
+
+    res.json({
+
+        body: req.body
+
+    });
+
+});
+
 app.listen(port, function () {
 
     console.log('micro_img_main is up on port: ' + port);
--- projects/micro_img_main/public/js/client.js
+++ projects/micro_img_main/public/js/client.js
@@ -1,1 +1,21 @@
-console.log('okay');
+//var imgReady = function (a, b) {
+//    console.log(this.files[0]);
+//};
+//$('#imgInp').change(imgReady);
+
+
+$('#imgSend').on('click', function (e) {
+
+    var files = $('#imgInp')[0].files;
+
+    if (files.length > 0) {
+
+        console.log(files[0]);
+
+    } else {
+
+        console.log('no file');
+
+    }
+
+});
