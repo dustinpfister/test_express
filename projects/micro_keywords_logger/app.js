204e1f467fc5a8993b382c95d05552c3c17b227f
dustinpfister <dustin.pfister@gmail.com>
2018-06-07 13:28:47 -0400

    post method

--- projects/micro_keywords_logger/app.js
+++ projects/micro_keywords_logger/app.js
@@ -6,14 +6,35 @@ port = process.env.PORT || process.argv[2] || 8080,
 kw,
 app = express();
 
+// using the body-parser json to parse incoming
+// post requests with keyword json data
+app.use(require('body-parser').json());
+
+// GET requests at /
 app.get('/', function (req, res) {
 
+    // just serve up the whole database for now
     res.json(kw.get('keywords').value());
 
 });
 
+// POST incoming keywords
+app.post('/', function (req, res) {
+
+    console.log(req.body);
+
+    res.json({
+        success: true,
+        mess: 'post recived',
+        term: req.body
+    });
+
+});
+
+// make sure db folder is there
 mkdirp('db', function () {
 
+    // create keywords.json if it is not there
     kw = low(new FileSync('db/keywords.json'));
 
     // default to empty keyword array
@@ -21,6 +42,7 @@ mkdirp('db', function () {
         keywords: []
     }).write();
 
+    // start service with the value of port
     app.listen(port, function () {
 
         console.log('micro_keywords_logger is up on port: ' + port);
