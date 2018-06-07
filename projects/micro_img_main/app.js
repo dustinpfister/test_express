24db52f13c65d678f29ac8a84c0ee3f2989328d7
dustinpfister <dustin.pfister@gmail.com>
2018-06-07 10:28:37 -0400

    posting

--- projects/micro_img_main/app.js
+++ projects/micro_img_main/app.js
@@ -1,4 +1,5 @@
 let express = require('express'),
+http = require('http'),
 
 port = process.env.PORT || process.argv[2] || 8888,
 app = express();
@@ -15,7 +16,70 @@ app.use(require('body-parser').raw({
 
 app.post('/post', function (req, res) {
 
-    console.log(req.body.length);
+    console.log(typeof req.body);
+
+    if (req.body) {
+
+        let req = http.request({
+
+                host: 'http://localhost',
+                port: 8080,
+                method: 'POST'
+
+            });
+
+        req.on('error', function (e) {
+
+            res.json({
+
+                mess: 'okay error',
+                success: true
+
+            });
+
+        });
+
+        req.on('end', function (e) {
+
+            res.json({
+
+                mess: 'um... i think we did it',
+                success: true
+
+            });
+
+        });
+
+        req.write('foo');
+        req.end();
+
+        /*
+        try {
+        req.send();
+
+        } catch (e) {
+
+        res.json({
+
+        mess: 'error tring to send to microtservice',
+        success: false
+
+        });
+
+        }
+         */
+
+    } else {
+
+        // no body
+        res.json({
+
+            success: false,
+            mess: 'no body'
+
+        });
+
+    }
 
     /*
     req.on('data', function (chunk) {
@@ -23,14 +87,16 @@ app.post('/post', function (req, res) {
     console.log(chunk.length);
 
     });
-	*/
-    
+     */
 
+    /*
     res.json({
 
-        mess: 'check cmd'
+    mess: 'check cmd'
 
     });
+
+     */
 
 });
 
