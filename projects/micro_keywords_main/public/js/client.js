0ac925435e31dfa865d6d35116726e92cda707c2
dustinpfister <dustin.pfister@gmail.com>
2018-06-09 08:45:10 -0400

    disp div added

--- projects/micro_keywords_main/public/js/client.js
+++ projects/micro_keywords_main/public/js/client.js
@@ -11,11 +11,21 @@ $('#search_submit').on('click', function () {
             keyword: keyword
 
         }),
-		contentType: 'application/json',
+        contentType: 'application/json',
 
     }).done(function (res) {
 
         console.log(res);
+
+        if (res.success) {
+
+            $('#disp').get(0).innerHTML = res;
+
+        } else {
+
+            $('#disp').get(0).innerHTML = res.mess;
+
+        }
 
     })
 
