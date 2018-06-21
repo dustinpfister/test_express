20815175d729eecdd7d08fea1107ae8431f243d9
dustinpfister <dustin.pfister@gmail.com>
2018-06-21 10:29:02 -0400

    new demo on app.delete

---
+++ demos/app-delete/app.js
@@ -0,0 +1,16 @@
+let express = require('express'),
+
+app = express();
+
+// find out the port to listen on
+app.set('port', process.argv[2] || process.env.PORT || 8080);
+
+// serve a static client
+app.use('/', express.static('public'));
+
+// start listening
+app.listen(app.get('port'), function () {
+
+    console.log('the demo is up on port: ' + app.get('port'));
+
+});
