596c55a01730f9697ee2d1541c079bba4792b53e
dustinpfister <dustin.pfister@gmail.com>
2018-06-22 11:32:57 -0400

    looks good

--- demos/response-app/app.js
+++ demos/response-app/app.js
@@ -1,9 +1,13 @@
 let express = require('express'),
-
+path = require('path'),
 // the main app instance
 app = express();
 app.set('id','main');
 app.set('port',8080);
+
+// path and view engine
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'ejs');
 
 app.use(require('./otherapp.js'));
 
--- demos/response-app/otherapp.js
+++ demos/response-app/otherapp.js
@@ -1,5 +1,27 @@
-module.exports = function (req,res) {
-    res.json({
-        appID : res.app.get('id')
+let express = require('express'),
+renderApp = express();
+
+module.exports = function (req, res) {
+
+    let app = res.app;
+
+    // copy over settings from main app
+    // including views path, and engine
+    Object.keys(app.locals.settings).forEach(function (key) {
+        renderApp.set(key, app.get(key));
+    });
+
+    // preserve id
+    renderApp.set('id', 'render');
+
+    res.render('index', {
+
+        settings: {
+
+            app: app.locals.settings,
+            renderApp: renderApp.locals.settings
+
+        }
+
     });
 };
