47aff68614b1e1f9d399b41f13fd60ec7d59a8ef
dustinpfister <dustin.pfister@gmail.com>
2018-11-26 17:06:34 -0500

    basic example working

--- demos/body-parser/basic.js
+++ demos/body-parser/basic.js
@@ -9,11 +9,13 @@ app.use(require('body-parser').json());
 
 app.get('/', function (req, res) {
 
-    res.send('<script>var xhr = new XMLHttpRequest();xhr.open(\'POST\', \'/\', true);xhr.onreadystatechange = function(){console.log(this)};xhr.send();</script>');
+    res.send('<script>var xhr = new XMLHttpRequest();xhr.open(\'POST\', \'/\', true);xhr.onreadystatechange = function(){console.log(this)};xhr.setRequestHeader(\'Content-type\', \'application/json\');xhr.send(JSON.stringify({foo:\'bar\'}));</script>');
 
 });
 
 app.post('/', function (req, res) {
+
+    console.log(req.body); // {foo:'bar'}
 
     res.send('foo');
 
