b2f8369330f2185c0b2da37f8f8c12c4f41bd7e7
dustinpfister <dustin.pfister@gmail.com>
2018-11-26 17:14:06 -0500

    basic demo clean up

--- demos/body-parser/basic.js
+++ demos/body-parser/basic.js
@@ -5,24 +5,35 @@ app = express(),
 // getting port this way
 port = process.env.PORT || process.argv[2] || 8080;
 
+// using the JSON body parser
 app.use(require('body-parser').json());
 
+// what to do for get requests to root
 app.get('/', function (req, res) {
 
-    res.send('<script>var xhr = new XMLHttpRequest();xhr.open(\'POST\', \'/\', true);xhr.onreadystatechange = function(){console.log(this)};xhr.setRequestHeader(\'Content-type\', \'application/json\');xhr.send(JSON.stringify({foo:\'bar\'}));</script>');
+    // sending a crude yet effective client system
+    res.send('<script>' +
+        'var xhr = new XMLHttpRequest();' +
+        'xhr.open(\'POST\', \'/\', true);' +
+        'xhr.onreadystatechange = function(){console.log(this.response)};' +
+        'xhr.setRequestHeader(\'Content-type\', \'application/json\');' +
+        'xhr.send(JSON.stringify({foo:\'bar\',n:40}));</script>');
 
 });
 
+// what to do for post requests to root
 app.post('/', function (req, res) {
 
-    console.log(req.body); // {foo:'bar'}
+    // body parser works, I have the object sent
+    //from the client system
+    console.log(req.body); // {foo:'bar',n:40}
 
-    res.send('foo');
+    // I can do something with it and send a response
+    req.body.n += 2;
+    res.json(req.body);
 
 })
 
 app.listen(port, function () {
-
-    console.log('request object demo is up on port: ' + port);
-
+    console.log('body-parser demo is up on port: ' + port);
 });
