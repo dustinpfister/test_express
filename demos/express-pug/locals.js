26d2eef693631530f3c4e69c45add0002743fef4
dustinpfister <dustin.pfister@gmail.com>
2019-04-16 11:03:23 -0400

    commnets

--- demos/express-pug/locals.js
+++ demos/express-pug/locals.js
@@ -3,6 +3,9 @@ app = express();
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
+    // an object can be passed to the render
+    // method that will pass local variables
+    // than can then be used in the pug file
     res.render('locals', {
         foo: 'bar',
         n: 42
