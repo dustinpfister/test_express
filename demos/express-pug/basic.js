6605cf300ba67c49c2a9b811a9a2993dd3835e28
dustinpfister <dustin.pfister@gmail.com>
2019-04-16 09:40:20 -0400

    comments

--- demos/express-pug/basic.js
+++ demos/express-pug/basic.js
@@ -1,12 +1,17 @@
+// get express and create an
+// express app object
 let express = require('express'),
 app = express();
 
+// set pug as the view engine
 app.set('view engine', 'pug');
 
+// use the render method to render
+// a pug template
 app.get('/', (req, res) => {
-
-    res.render('index', {});
-
+    res.render('index');
 });
 
+// use listen or createServer to
+// listen on a port
 app.listen(8080);
