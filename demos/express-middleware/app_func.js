9e735506d065d3079eac49749e252e297f92c19f
dustinpfister <dustin.pfister@gmail.com>
2019-04-19 18:02:28 -0400

    express-middleware

---
+++ demos/express-middleware/app_func.js
@@ -0,0 +1,18 @@
+let express = require('express'),
+
+app = express();
+
+
+app.use((req, res, next) => {
+    // populating something  on req
+    req.plat = {
+        platform: process.platform,
+        arch: process.arch
+    };
+    // call next to continue
+    next();
+});
+
+app.get('/', (req, res) => res.json(req.plat));
+
+app.listen(8080);
