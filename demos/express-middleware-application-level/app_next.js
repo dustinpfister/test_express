e84b5ff3484fdeaa8491cde3154f9d752ef74c44
dustinpfister <dustin.pfister@gmail.com>
2019-04-20 10:52:59 -0400

    app_next

---
+++ demos/express-middleware-application-level/app_next.js
@@ -0,0 +1,18 @@
+let express = require('express'),
+app = express();
+
+app.get('*', (req, res, next) => {
+
+    console.log('incoming get request for path: ' + req.path);
+
+    next();
+
+});
+
+app.get('/', (req, res) => {
+
+    res.send('foo');
+
+});
+
+app.listen(8080);
