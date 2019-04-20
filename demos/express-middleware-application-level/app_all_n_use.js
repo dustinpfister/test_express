c0a5c90018deda4f3fc51559c5ff862142b2acc3
dustinpfister <dustin.pfister@gmail.com>
2019-04-20 11:07:41 -0400

    app_all_n_use

---
+++ demos/express-middleware-application-level/app_all_n_use.js
@@ -0,0 +1,17 @@
+let express = require('express'),
+app = express();
+
+app.use((req, res, next) => {
+
+    req.n = 42;
+    next();
+
+});
+
+app.all('*', (req, res, next) => {
+
+    res.send(req.n + '');
+
+});
+
+app.listen(8080);
