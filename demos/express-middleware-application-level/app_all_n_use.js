9e1281f36ed584719712442031af11e6ca19b659
dustinpfister <dustin.pfister@gmail.com>
2019-04-22 16:22:11 -0400

    app_all_n_use better example

--- demos/express-middleware-application-level/app_all_n_use.js
+++ demos/express-middleware-application-level/app_all_n_use.js
@@ -1,13 +1,30 @@
 let express = require('express'),
 app = express();
 
-app.use((req, res, next) => {
+app.use('/foo', (req, res, next) => {
 
-    req.n = 42;
+    req.a = 40;
     next();
 
 });
 
-app.all('*', (req, res, next) => res.send(req.n + ''));
+app.all('/foo', (req, res, next) => {
+
+    req.b = 2;
+    next();
+
+});
+
+app.all('*', (req, res, next) => {
+
+    req.a = req.a || 0;
+    req.b = req.b || 0;
+
+    res.send(req.a + req.b + '');
+});
 
 app.listen(8080);
+
+// http://localhost:8080/         <-- 0
+// http://localhost:8080/foo      <-- 42
+// http://localhost:8080/foo/bar  <-- 40
