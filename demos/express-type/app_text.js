f67f7deb689d5c346f2fa1eca7b838837aa35bf8
dustinpfister <dustin.pfister@gmail.com>
2019-04-24 13:18:45 -0400

    express type

---
+++ demos/express-type/app_text.js
@@ -0,0 +1,12 @@
+let express = require('express'),
+app = express();
+
+app.get('/', (req, res) => {
+
+    res.type('text');
+    console.log(res.get('Content-Type')); // 'text/plain; charset=utf-8'
+    res.send('<h1>This should just be plain text so the tags should show in the browser</h1>')
+
+});
+
+app.listen(8080);
