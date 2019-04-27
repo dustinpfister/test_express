e8b209020e6dfe515acd02ca34a8ab4d48e4fd2e
dustinpfister <dustin.pfister@gmail.com>
2019-04-27 17:26:34 -0400

    express-response-objects

---
+++ demos/express-response-objects/app_res_set.js
@@ -0,0 +1,21 @@
+let express = require('express'),
+app = express();
+
+// using res.set to set the Content-Type header
+// for a javaScript resource
+app.get('/js/foo.js', (req, res) => {
+    res.set('Content-Type', 'application/javascript');
+    res.send('var foo = document.createElement(\'p\');foo.innerHTML=\'foo\';document.body.appendChild(foo);');
+});
+
+// using res.set to set more than one response header
+// at once
+app.get('/', (req, res) => {
+    res.set({
+        'Content-Type': 'text/html',
+        'Cache-Control': 'no-store'
+    });
+    res.send('<body><script src=\"\/js\/foo.js\"><\/script><\/body>');
+});
+
+app.listen(8080);
