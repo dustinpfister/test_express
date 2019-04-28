c849c773994e9b6a8f6d5e5f7251c4f868767f35
dustinpfister <dustin.pfister@gmail.com>
2019-04-28 08:43:56 -0400

    app_status

---
+++ demos/express-end/app_status.js
@@ -0,0 +1,19 @@
+let express = require('express'),
+app = express();
+
+// the res.end method does nothing to change
+// the default http status code of 200 (OK)
+app.get('/foo', (req, res) => {
+    console.log(res.statusCode); // 200
+    res.end();
+    console.log(res.statusCode); // 200
+});
+
+// the res.status method can be used to change that
+app.get('*', (req, res) => {
+    console.log(res.statusCode); // 200
+    res.status(404).end();
+    console.log(res.statusCode); // 403
+});
+
+app.listen(8080);
