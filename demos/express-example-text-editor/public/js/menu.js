f02ec0fda7cff2adafe606db7880367b2a2df34f
dustinpfister <dustin.pfister@gmail.com>
2019-05-04 10:00:09 -0400

    new system working okay so far

--- demos/express-example-text-editor/public/js/menu.js
+++ demos/express-example-text-editor/public/js/menu.js
@@ -7,14 +7,13 @@ var Menu = (function () {
     api.done = function (text) {
         console.log(text)
     };
-    api.error = function (e) {
-        console.log(e.message)
-    };
+    api.error = function (eMess) {
+        get('text_mess').innerHTML = eMess;
+    }
 
     // Open the file that is at the current
     // dir and fn app settings
     api.Open = function (opt) {
-
         // if null for dir or fn the default will
         // be whatever is set server side
         opt = opt || {};
@@ -23,15 +22,12 @@ var Menu = (function () {
                 action: 'open',
                 dir: opt.dir || null,
                 fn: opt.fn || null
-
-            }
-            //action: 'open',
-            //dir: opt.dir || null,
-            //fn: opt.fn || null,
-            //onDone: opt.onDone || api.done,
-            //onError: opt.onError || api.error
+            },
+            onDone: function (text) {
+                get('text_edit').value = text;
+            },
+            onError: api.error
         });
-
     }
 
     return api;
