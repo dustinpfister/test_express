0c9768166c9ca31ba0d53da80996063d94c35c99
dustinpfister <dustin.pfister@gmail.com>
2019-05-04 15:20:18 -0400

    get json

--- demos/express-json/app_send_json.js
+++ demos/express-json/app_send_json.js
@@ -1,17 +1,13 @@
 let express = require('express'),
 app = express();
 app.set('port', process.env.PORT || process.argv[2] || 8080);
-
 // path for root
 app.get('/', (req, res) => {
-
     // The res.json response method can be used to
     // send json to a client
     res.json({
         foo: 'bar'
     });
-
 });
-
 // listen
 app.listen(app.get('port'), () => console.log('app up on port: ' + app.get('port')));
