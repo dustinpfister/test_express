2c0ca4f94bbe4d3a7c50a31075f7c8cfd0a97d5b
dustinpfister <dustin.pfister@gmail.com>
2019-05-06 21:08:36 -0400

    markdown editor is so far just a copy of the text editor

--- demos/express-example-markdown-editor/app.js
+++ demos/express-example-markdown-editor/app.js
@@ -5,19 +5,130 @@ app = express();
 
 app.set('port', process.argv[2] || process.env.PORT || 8080);
 
+app.set('dir', path.join(process.cwd(), '_posts'));
+app.set('fn', 'first-post.md');
+app.set('encode', 'utf8');
 
-// using express static to host the index.html file
-app.use('/', express.static('public'));
+// hosting static assets for the client system
+app.use('/js', express.static('public/js'));
+app.use('/', express.static('public/html'));
 
-// using body parser to parse incoming json from
-// the client
+// body parser
 app.use(require('body-parser').json());
 
 // singe middleware that responds to post requests
-app.post('/data', (req, res) => {
-    res.json({
-        mess: req.body.mess + 'bar'
-    });
-});
+app.post('/data',
 
-app.listen(app.get('port'), () => console.log('example markdown editor is up on port: ' + app.get('port')));
+    [
+
+        // create reply object, and check for body
+        (req, res, next) => {
+
+            // Create reply object
+            res.reply = {
+                success: false,
+                mess: 'no body object populated.',
+                //fn: app.get('fn'),
+                //dir: app.get('dir')
+            };
+
+            // check for body or next
+            if (!req.body) {
+                res.status(400).json(res.reply);
+            } else {
+                // sync server side fn and dir settings to any settings given by client
+                app.set('fn', req.body.fn || app.get('fn'));
+                app.set('dir', req.body.dir || app.get('dir'));
+                res.reply.fn = app.get('fn');
+                res.reply.dir = app.get('dir');
+                // next middleware
+                next();
+            }
+        },
+
+        // if no action
+        (req, res, next) => {
+            // If no action Action, or next
+            if (!req.body.action) {
+                res.reply.mess = 'An action must be given';
+                res.status(400).json(res.reply);
+            } else {
+                next();
+            }
+        },
+
+        // if action : 'open'
+        (req, res, next) => {
+            if (req.body.action === 'open') {
+                // try to open the current filename at the current dir
+                fs.readFile(path.join(app.get('dir'), app.get('fn')), app.get('encode'), (e, data) => {
+                    if (e) {
+                        res.reply.mess = e.message;
+                        res.status(400).json(res.reply);
+                    } else {
+                        res.reply.success = true;
+                        res.reply.mess = 'opened and sent file data';
+                        res.reply.data = data;
+                        res.status(200).json(res.reply);
+                    }
+                });
+            } else {
+                next();
+            }
+        },
+
+        // if action : 'save'
+        (req, res, next) => {
+            if (req.body.action === 'save') {
+                // if we have data
+                if (req.body.data) {
+                    // try to save the data
+                    fs.writeFile(path.join(app.get('dir'), app.get('fn')), req.body.data, (e) => {
+                        if (e) {
+                            res.reply.mess = e.message;
+                            res.status(400).json(res.reply);
+                        } else {
+                            res.reply.success = true;
+                            res.reply.mess = 'save file success';
+                            res.reply.data = req.body.data;
+                            res.status(200).json(res.reply);
+                        }
+                    })
+                } else {
+                    // else we do not have data to save
+                    res.reply.mess = 'must have data to save';
+                    res.status(400).json(res.reply);
+                }
+            } else {
+                next();
+            }
+        },
+
+        // if action : 'list' - to list files in current dir
+        (req, res, next) => {
+            if (req.body.action === 'list') {
+                fs.readdir(path.resolve(app.get('dir')), (e, files) => {
+                    if (e) {
+                        res.reply.mess = e.message;
+                        res.status(400).json(res.reply);
+                    } else {
+                        res.reply.success = true;
+                        res.reply.mess = 'list sent';
+                        res.reply.data = files;
+                        res.status(200).json(res.reply);
+                    }
+                });
+            } else {
+                next();
+            }
+        },
+
+        // unknown action
+        (req, res, next) => {
+            res.reply.mess = 'The action given is not known';
+            res.status(400).json(res.reply);
+        }
+
+    ]);
+
+app.listen(app.get('port'), () => console.log('example text editor is up on port: ' + app.get('port')));
---
+++ demos/express-example-markdown-editor/public/js/menu.js
@@ -0,0 +1,128 @@
+// Module to help work with the
+// back end system for the editor
+var Menu = (function () {
+
+    // set / clear messages
+    var mess = (function () {
+        var el_mess = get('text_mess'),
+        el_eMess = get('text_emess');
+        var func = function (mess) {
+            el_mess.innerHTML = mess;
+        };
+        func.eMess = function (eMess) {
+            el_eMess.innerHTML = eMess;
+        };
+        func.clear = function () {
+            el_mess.innerHTML = '';
+            el_eMess.innerHTML = '';
+        };
+        return func;
+    }
+        ());
+
+    // set the dir and fn input elements values to
+    // what is in the given reply object
+    var setInputs = function (reply) {
+        get('text_dir').value = reply.dir;
+        get('text_fn').value = reply.fn;
+    };
+
+    // public api
+    var api = {};
+    api.noop = function () {};
+    api.done = function (text) {
+        console.log(text)
+    };
+    api.error = function (eMess) {
+        console.log(eMess);
+        mess.eMess(eMess);
+    }
+
+    // Open the file that is at the current
+    // dir and fn app settings
+    api.Open = function (opt) {
+        // if null for dir or fn the default will
+        // be whatever is set server side
+        opt = opt || {};
+        mess.clear();
+        get({
+            payload: {
+                action: 'open',
+                dir: opt.dir || null,
+                fn: opt.fn || null
+            },
+            onDone: function (text, resObj) {
+                get('text_edit').value = text;
+                //get('text_fn').value = resObj.fn;
+                mess(resObj.mess);
+                setInputs(resObj);
+            },
+            onError: api.error
+        });
+    };
+
+    api.Save = function (opt) {
+
+        opt = opt || {};
+        mess.clear();
+        get({
+            payload: {
+                action: 'save',
+                dir: opt.dir || null,
+                fn: opt.fn || null,
+                data: get('text_edit').value
+            },
+            onDone: function (text, resObj) {
+                get('text_edit').value = text;
+                mess(resObj.mess);
+            },
+            onError: api.error
+        });
+
+    };
+
+    var emptyList = function () {
+        var list = get('list');
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+    };
+
+    api.List = function (opt) {
+
+        opt = opt || {};
+        mess.clear();
+        get({
+            payload: {
+                action: 'list',
+                dir: opt.dir || null,
+            },
+            onDone: function (files, resObj) {
+
+                emptyList();
+
+                //var html = '<br>';
+                var list = get('list');
+                files.forEach(function (fn) {
+                    //html += '<span>' + fn + '<\/span><br>';
+                    var item = document.createElement('p');
+                    item.innerText = fn;
+                    item.addEventListener('click', function (e) {
+                        // open the file clicked
+                        api.Open({
+                            fn: e.target.innerText
+                        })
+                    })
+                    list.appendChild(item);
+
+                });
+            },
+            onError: api.error
+        });
+
+    };
+
+    return api;
+
+}
+    ());
