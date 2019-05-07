23a4397837043367096286e4428640ef4142e89a
dustinpfister <dustin.pfister@gmail.com>
2019-05-07 10:34:05 -0400

    file renamed

--- demos/express-example-markdown-editor/app.js
+++ demos/express-example-markdown-editor/app.js
@@ -14,7 +14,7 @@ app.use('/js', express.static('public/js'));
 app.use('/', express.static('public/html'));
 
 // html of current md file
-app.use('/html', require('./middleware/post_gethtml.js'));
+app.use('/html', require('./middleware/md_html.js'));
 
 // body parser
 app.use(require('body-parser').json());
