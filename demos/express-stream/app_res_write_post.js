a1613e6a1f55df149f9e5b8c227ee71c4b3f8b39
dustinpfister <dustin.pfister@gmail.com>
2019-08-09 14:14:10 -0400

    okay now I have something

--- demos/express-stream/app_res_write_post.js
+++ demos/express-stream/app_res_write_post.js
@@ -1,28 +1,37 @@
 let express = require('express'),
 fs = require('fs'),
 app = express(),
-file = process.argv[2] || 'package.json',
+file = process.argv[2] || 'file.txt',
 port = process.argv[3] || 8080;
 
 app.use(express.static('./public'));
 
 app.post('/', (req, res) => {
 
-
-    fs.stat(file, (e,stat) => {
+    fs.stat(file, (e, stat) => {
 
         res.writeHead(200, {
             'Content-Length': stat.size,
             'Content-Type': 'text/plain'
         });
 
-        let reader = fs.createReadStream(file);
+        let reader = fs.createReadStream(file, {
+                highWaterMark: 256
+            });
 
         reader.on('close', () => {
             res.end();
         });
 
-        reader.pipe(res);
+        reader.on('data', (data) => {
+
+            console.log(data.length);
+
+            res.write(data);
+
+        });
+
+        //reader.pipe(res);
 
     });
 
