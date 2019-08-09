8976325113f5d14127fc1364bacc8ff3e4da80a6
dustinpfister <dustin.pfister@gmail.com>
2019-08-09 14:00:06 -0400

    get stats

--- demos/express-stream/app_res_write_post.js
+++ demos/express-stream/app_res_write_post.js
@@ -1,15 +1,30 @@
 let express = require('express'),
+fs = require('fs'),
 app = express(),
-port = process.argv[2] || 8080;
+file = process.argv[2] || 'package.json',
+port = process.argv[3] || 8080;
 
 app.use(express.static('./public'));
 
 app.post('/', (req, res) => {
 
-    res.write('foobar');
-    res.write('foobar');
-    res.write('foobar');
-    res.end();
+
+    fs.stat(file, (e,stat) => {
+
+        res.writeHead(200, {
+            'Content-Length': stat.size,
+            'Content-Type': 'text/plain'
+        });
+
+        let reader = fs.createReadStream(file);
+
+        reader.on('close', () => {
+            res.end();
+        });
+
+        reader.pipe(res);
+
+    });
 
 });
 
