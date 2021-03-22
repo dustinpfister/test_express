20863b7a22744a2a81f732968d552287df211508
dustinpfister <dustin.pfister@gmail.com>
2021-03-22 13:52:10 -0400

    public folder for serve-index example

---
+++ demos/express-example-serve-index/public/js/foo.js
@@ -0,0 +1,7 @@
+var container = document.body;
+
+var foo = document.createElement('p');
+
+foo.innerText = 'javaScript works';
+
+container.appendChild(foo);
--- demos/express-example-serve-index/static.js
+++ demos/express-example-serve-index/static.js
@@ -7,7 +7,8 @@ PORT = process.env.PORT || process.argv[2] || 8080,
 PUBLIC_HTML = path.resolve(__dirname, 'public');
 
 // use serve index to nav public folder
-app.use('/', serveIndex( path.resolve(PUBLIC_HTML) ))
+app.use('/', serveIndex( path.resolve(PUBLIC_HTML) ));
+
 // use express static to serve public folder assets
 app.use('/', express.static( path.join(PUBLIC_HTML) ));
 
