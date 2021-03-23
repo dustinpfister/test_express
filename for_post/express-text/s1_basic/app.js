54d18021a9b70c1eab5e1ea6f38d106322185da1
dustinpfister <dustin.pfister@gmail.com>
2021-03-23 13:28:17 -0400

    comments

--- for_post/express-text/s1_basic/app.js
+++ for_post/express-text/s1_basic/app.js
@@ -1,24 +1,21 @@
 let express = require('express'),
 path = require('path'),
 app = express();
-
 app.set('port', process.argv[2] || process.env.PORT || 8080);
 
+// using express satic to host my single index.html file
 app.use('/', express.static( path.join(__dirname, 'public') ))
 
+// using express.text to parse post requests for the root path
 app.use('/', express.text());
 app.post('/', (req, res) => {
-
-    console.log(req.body);
-
     res.json({
         status: 'okay',
         body: req.body
     });
-
 });
 
-// listen
+// listen on app port
 app.listen(app.get('port'), () => {
     console.log('app up on port: ' + app.get('port'));
 });
