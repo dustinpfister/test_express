29c6ebbed0825a7ae24038c058d5452fba71a02d
dustinpfister <dustin.pfister@gmail.com>
2021-03-24 15:04:16 -0400

    all_requests.js file

--- for_post/express-next/s3_useragent/app.js
+++ for_post/express-next/s3_useragent/app.js
@@ -4,38 +4,8 @@ app = express();
 
 app.set('port', process.argv[2] || process.env.PORT || 8080);
 
-app.get('*', [
-    // log request url to the console
-    (req, res, next) => {
-        console.log(req.url);
-        next();
-    },
-    // send favicon file
-    (req, res, next) => {
-        // if the request is for /favicon, send a favicon.ico file for the request
-        if(req.url.toLowerCase() === '/favicon.ico'){
-            res.sendFile( path.join(__dirname, 'favicon.ico') );
-        }else{
-            next();
-        }
-    },
-    // set a req.platform prop based on user agent header
-    (req, res, next) => {
-        var userAgent = req.get('user-agent');
-        // default to an 'Unkown platform'
-        req.platform = 'Unkown';
-        if(userAgent.match(/\(X11/)){
-            req.platform = 'Linux';
-        }
-        if(userAgent.match(/\(Windows/)){
-            req.platform = 'Windows';
-        }
-        if(userAgent.match(/\(Macintosh/)){
-            req.platform = 'Macintosh';
-        }
-        next();
-    }
-]);
+// use all_requests.js for '*' path
+app.all('*', require( path.join(__dirname, 'all_requests.js') ))
 
 app.get('/', (req, res) => {
     res.send('Hello ' + req.platform + ' OS User');
