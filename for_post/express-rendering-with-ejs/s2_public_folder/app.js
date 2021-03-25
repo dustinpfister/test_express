578216123bdc9eb0b3d601fe7fa1edd8e799cab7
dustinpfister <dustin.pfister@gmail.com>
2021-03-25 13:58:35 -0400

    new public folder example

--- for_post/express-rendering-with-ejs/s2_public_folder/app.js
+++ for_post/express-rendering-with-ejs/s2_public_folder/app.js
@@ -4,19 +4,21 @@ app = express();
  
 // getting port this way
 app.set('port', process.env.PORT || process.argv[2] || 8080 );
-app.set('public_html', path.resolve( __dirname, 'public') ));
+app.set('public_html', path.resolve( __dirname, 'public') );
  
 // view engine setup
 app.set('view engine', 'ejs'); // the render engine
 app.set('views', path.resolve( __dirname, 'views') ); // the views folder for the *.ejs files
 
 // a single path for /
-app.get('/', [
-    express.static( api.get('public_html') ),
+app.use('/', [
+    // use express.static first to look for a static resource
+    express.static( app.get('public_html') ),
+    // if not found render main index
     function (req, res) {
         res.render('index', {}); 
     }
-);
+]);
 
 // listen on the port app setting
 app.listen(app.get('port'), function () {
