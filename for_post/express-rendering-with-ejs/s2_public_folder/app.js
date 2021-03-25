e3a8cf9f7ae5fbe9775f22ebcfe272fae980357a
dustinpfister <dustin.pfister@gmail.com>
2021-03-25 14:02:25 -0400

    404

--- for_post/express-rendering-with-ejs/s2_public_folder/app.js
+++ for_post/express-rendering-with-ejs/s2_public_folder/app.js
@@ -14,11 +14,19 @@ app.set('views', path.resolve( __dirname, 'views') ); // the views folder for th
 app.use('/', [
     // use express.static first to look for a static resource
     express.static( app.get('public_html') ),
-    // if not found render main index
-    function (req, res) {
-        res.render('index', {}); 
+    // if not found render main index, but only for / else next
+    function (req, res, next) {
+        if(req.url === '/'){
+            res.render('index', {});
+        }else{
+            next();
+        } 
     }
 ]);
+
+app.get('*', function(req, res){
+    res.render('404', {});
+});
 
 // listen on the port app setting
 app.listen(app.get('port'), function () {
