043adc9ca28f36e1bda2028381d9f67547f6f421
dustinpfister <dustin.pfister@gmail.com>
2021-03-26 13:39:36 -0400

    s2 example looks good

--- for_post/express-request-params/s2_folders/app.js
+++ for_post/express-request-params/s2_folders/app.js
@@ -3,29 +3,35 @@ path = require('path'),
 fs = require('fs'),
 promisify = require('util').promisify,
 readdir = promisify(fs.readdir),
+readFile = promisify(fs.readFile),
 app = express();
  
 // getting port this way
 app.set('port', process.env.PORT || process.argv[2] || 8080 );
 
 app.get('/', (req, res) => {
-   readdir(path.resolve(__dirname, 'examples'))
-   .then((folders) => {
-       let html = '';
-       folders.forEach((folderName)=>{
-           html += '<a href=\"/examples/' + folderName + '\">' + folderName + '</a><br>';
-       });
-       res.send(html);
-   })
-   .catch((e) => {
-      res.send(e.message);
-   });
-
+    readdir(path.resolve(__dirname, 'examples'))
+    .then((folders) => {
+        let html = '';
+        folders.forEach((folderName)=>{
+            html += '<a href=\"/examples/' + folderName + '\">' + folderName + '</a><br>';
+        });
+        res.send(html);
+    })
+    .catch((e) => {
+        res.send(e.message);
+    });
 });
 
 // a single path for /
 app.get('/examples/:exampleName', (req, res) => {
-    res.send('example Name: ' + req.params.exampleName);
+    readFile(path.resolve(__dirname, 'examples', req.params.exampleName, 'outline.txt'))
+    .then((text)=>{
+        res.send('<pre>' + text + '</pre>');
+    })
+    .catch((e)=>{
+        res.send(e.message);
+    });
 });
 
 // listen on the port app setting
